71f5789acb9560b44ec2f319b708f46fdff73d04
Zequi20 <ezequiel234242@gmail.com>
2023-06-16 20:47:59 -0400

    creacion entidades empleado y jornada

--- mysite/control/models.py
+++ mysite/control/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Empleado(models.Model):
+    identificador = models.CharField(max_length=50, unique=True)
+    nombre = models.CharField(max_length=150, null=False)
+    departamento = models.CharField(max_length=100, null=False)
+    fecha_inicio = models.DateField(auto_now_add=True)
+    dias_trabajo = models.CharField(max_length=100, null=False)
+    TURNO_OPCIONES = (
+        ('dia', 'Día'),
+        ('tarde', 'Tarde'),
+        ('noche', 'Noche'),
+    )
+    turno = models.CharField(max_length=10, choices=TURNO_OPCIONES, null=False)
+    horario_entrada = models.TimeField(null=False)
+    horario_salida = models.TimeField(null=False)
+
+class Jornada(models.Model):
+    identificador = models.CharField(max_length=50, unique=True)
+    empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE,  null=False)
+    fecha = models.DateTimeField(null=False)
+    TIPO_MARCACION = (
+        ('entrada', 'Entrada'),
+        ('salida', 'Salida'),
+    )
+    tipo_marcacion = models.CharField(max_length=10, choices=TIPO_MARCACION, null=False)
+
+  
+
+    class Meta:
+        ordering = ['fecha']
